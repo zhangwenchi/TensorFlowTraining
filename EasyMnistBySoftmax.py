27711b9d9e9b6d295460ae510e9568b1c81fd0cd
zhangwenchi <410735628@qq.com>
2017-06-15 01:14:23 -0700

    My first try for tensorflow

---
+++ EasyMnistBySoftmax.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
+
+x = tf.placeholder('float', [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder('float', [None, 10])
+
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+for step in range(1000):
+    x_batch, y_batch = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: x_batch, y_: y_batch})
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+
+print(sess.run(accuracy,feed_dict={x: mnist.test.images, y_:mnist.test.labels}))
+
